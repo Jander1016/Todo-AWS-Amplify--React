5d684a168a868a549c5c01b3523e6ac51bf78645
Jander Gomez <jandergb.30@gmail.com>
2022-10-30 15:56:28 -0500

    update

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "reactamplify": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "reactamplifycf55ee8b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import { useEffect, useState } from "react";
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Text,
+} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
+  const [data, setData] = useState({ name: "", description: "" });
+  const [todos, setTodos] = useState([]);
+  const [tasks, setTasks] = useState([]);
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setData({ ...data, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      if (!data.name || !data.description) return;
+
+      const todo = { ...data };
+      setTodos([...todos, todo]);
+      setData({ name: "", description: "" });
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
+      console.log(tasks);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const loadtasks = async () => {
+    const result = await API.graphql(graphqlOperation(listTodos));
+    const listData = await result.data.listTodos.items;
+    setTasks(listData);
+  };
+
+  useEffect(() => {
+    loadtasks();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container ">
+      <div className="row">
+        <div className="col-md4 offset-md-4">
+				<Heading level={1}>Hello {user.username}</Heading>
+        <Button onClick={signOut}>Sign out</Button>
+          <Form onSubmit={(e) => handleSubmit(e)}>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Name Task</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name task"
+                id="name"
+                autoComplete="off"
+                value={data.name}
+                onChange={(e) => handleInputChange(e)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="description">
+              <Form.Label>Description Task</Form.Label>
+              <Form.Control
+                type="text"
+                autoComplete="off"
+                id="description"
+                value={data.description}
+                onChange={(e) => handleInputChange(e)}
+              />
+            </Form.Group>
+            <Button type="submit">Create task</Button>
+            {tasks
+              ? tasks.map((task) => (
+                  <article key={task.id}>
+                    <Heading level={5}>{task.name}</Heading>
+                    <Text as="span">{task.description}</Text>
+                  </article>
+                ))
+              : null}
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
